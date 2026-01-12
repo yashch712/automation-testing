0c02a0ece10cacce24129cf66de55715266667a3
yashch712 <yashch712@gmail.com>
2026-01-12 17:18:24 +0530

    CI-ready pytest project with clean structure

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,22 @@
-def test_login():
-    print("Login test executed successfully")
-    assert True
+# def test_login():
+#     print("Login test executed successfully")
+#     assert True
+
+# PyTest FIXTURES EXAMPLE:
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def setup():
+    driver = webdriver.Chrome()
+    driver.get("https://www.google.com")
+    yield driver        # test runs here
+    driver.quit()       # runs after test
+
+
+def test_google_title(setup):
+    driver = setup
+    assert "Google" in driver.title
+
+def test_url(setup):
+    assert "google.com" in setup.current_url
